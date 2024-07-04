c6bdb8d1689259f5620cfcbc87ece847dadf7eab
999gawkboyy <mingeun3669@gmail.com>
2024-07-04 20:58:01 +0900

    test 2

--- A_module/js/app.js
+++ A_module/js/app.js
@@ -1,6 +1,28 @@
-const modal = document.querySelector("dialog")
-const closeBtn = document.querySelector(".closeBtn");
+// const modal = document.querySelector("dialog")
+// const closeBtn = document.querySelector(".closeBtn");
 
-closeBtn.addEventListener("click", () => {
-  modal.style.display = "none"
+// closeBtn.addEventListener("click", () => {
+//   modal.style.display = "none"
+// })
+
+const slide = document.querySelector(".slide")
+const back = document.querySelector(".back");
+const go = document.querySelector(".go");
+const slideTxT = document.querySelector(".slideCount > span")
+
+back.addEventListener("click", () => {
+  let i = 1;
+  if (i == 3) {
+    i = 0;
+  }
+  slide.style.marginLeft = `${450 * i}px`;
+  i++;
+});
+go.addEventListener("click", () => {
+  let i = 1;
+  if (i == 3) {
+    i = 0;
+  }
+  slide.style.marginLeft = `-${1000 * i}px`;
+  i++;
 })
